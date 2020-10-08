bc9b68573c1327a99ebf8bb2d3ce840494d787da
BenBiney <72565324+BenBiney@users.noreply.github.com>
2020-10-08 20:27:56 +0100

    Update Maximum scores.js

--- Maximum scores.js
+++ Maximum scores.js
@@ -1,1 +1,9 @@
-const MaximumScoresList = document.getElementById("MaximumScoresList");
+
+const highScoresList = document.getElementById("highScoresList");
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+
+highScoresList.innerHTML = highScores
+  .map(score => {
+    return `<li class="high-score">${score.name} - ${score.score}</li>`;
+  })
+  .join("");
